d17974874eda4b1986446fbba9e24697656bca6a
Ilya Kazakov <robotbotyourube@gmail.com>
2024-12-18 17:43:59 +0300

    Add ViewModel into BankAccountFragment

--- app/src/main/java/com/robotbot/financetracker/presentation/BankAccountFragment.kt
+++ app/src/main/java/com/robotbot/financetracker/presentation/BankAccountFragment.kt
@@ -1,17 +1,41 @@
 package com.robotbot.financetracker.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.robotbot.financetracker.FinanceTrackerApp
 import com.robotbot.financetracker.databinding.FragmentBankAccountsBinding
+import com.robotbot.financetracker.presentation.bank_account_adapter.BankAccountsAdapter
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class BankAccountFragment : Fragment() {
+
+    private val component by lazy {
+        (requireActivity().application as FinanceTrackerApp).component
+    }
 
     private var _binding: FragmentBankAccountsBinding? = null
     private val binding: FragmentBankAccountsBinding
         get() = _binding ?: throw RuntimeException("FragmentBankAccountsBinding == null")
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    @Inject
+    lateinit var bankAccountsAdapter: BankAccountsAdapter
+
+    private lateinit var viewModel: BankAccountViewModel
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +48,15 @@ class BankAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            viewModelFactory)[BankAccountViewModel::class.java]
+        lifecycleScope.launch {
+            viewModel.bankAccounts.collect {
+                bankAccountsAdapter.submitList(it)
+            }
+        }
+        binding.rvBankAccounts.adapter = bankAccountsAdapter
     }
 
     override fun onDestroyView() {
