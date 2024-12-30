f4a4708975d7bd3ff83a740c3521ce16af5c59aa
Ilya Kazakov <robotbotyourube@gmail.com>
2024-12-30 20:26:48 +0300

    Add "add category" button at the end of recycler view

--- app/src/main/java/com/robotbot/financetracker/presentation/category_adapter/CategoryAdapter.kt
+++ app/src/main/java/com/robotbot/financetracker/presentation/category_adapter/CategoryAdapter.kt
@@ -10,6 +10,19 @@ import javax.inject.Inject
 class CategoryAdapter @Inject constructor() :
     ListAdapter<TransactionCategoryEntity, CategoryViewHolder>(CategoryDiffCallback) {
 
+    private companion object {
+        const val VIEW_CATEGORY_TYPE = 0
+        const val VIEW_ADD_BUTTON_TYPE = 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == currentList.size) VIEW_ADD_BUTTON_TYPE else VIEW_CATEGORY_TYPE
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size + 1 // Add "Add category" button
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val binding = ItemCategoryBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -20,6 +33,17 @@ class CategoryAdapter @Inject constructor() :
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-
+        if (getItemViewType(position) == VIEW_CATEGORY_TYPE) {
+            val category = getItem(position)
+            with(holder.binding) {
+                with(category) {
+                    tvCategoryTitle.text = name
+                }
+            }
+        } else if (getItemViewType(position) == VIEW_ADD_BUTTON_TYPE) {
+            with(holder.binding) {
+                tvCategoryTitle.text = "Add category"
+            }
+        }
     }
 }
