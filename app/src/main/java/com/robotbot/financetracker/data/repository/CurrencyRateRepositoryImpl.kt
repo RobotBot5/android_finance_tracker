b8d8e45b42241ba9f3e0469dacf3659bfc9dd1e3
Ilya Kazakov <robotbotyourube@gmail.com>
2025-01-11 18:36:23 +0300

    Remove saving currency rates in CurrencyRateRepositoryImpl

--- app/src/main/java/com/robotbot/financetracker/data/repository/CurrencyRateRepositoryImpl.kt
+++ app/src/main/java/com/robotbot/financetracker/data/repository/CurrencyRateRepositoryImpl.kt
@@ -11,18 +11,12 @@ class CurrencyRateRepositoryImpl @Inject constructor(
     private val apiService: ApiService
 ) : CurrencyRateRepository {
 
-    private var currencyRates: Map<String, Double>? = null
-
     override suspend fun getCurrencyRates(): Map<String, Double> {
-        val currentList = currencyRates
-        if (currentList != null) return currentList
         val response = apiService.getCoinPrices()
         return mapOf(
             Currency.USD.name to response.usd.rub,
             Currency.EUR.name to response.eur.rub,
             Currency.BTC.name to response.btc.rub
-        ).also {
-            currencyRates = it
-        }
+        )
     }
 }
